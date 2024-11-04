c4f4b499d140cd918e81a29fcbf1c777cea8acc3
Kirill Gusev <myaumuraa@gmail.com>
2024-11-04 23:56:37 +0300

    add sound effects

--- main.gd
+++ main.gd
@@ -10,11 +10,14 @@ func _process(delta: float) -> void:
 	pass
 
 func game_over() -> void:
+	$Music.stop()
+	$DeathSound.play()
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
 
 func new_game() -> void:
+	$Music.play()
 	get_tree().call_group("mobs", "queue_free")
 	score = 0
 	$Player.start()
@@ -24,20 +27,15 @@ func new_game() -> void:
 
 func _on_mob_timer_timeout() -> void:
 	var mob = mob_scene.instantiate()
-	
 	var mob_spawn_location = $MobPath/MobSpawnLocation
-	mob_spawn_location.progress_ratio = randf()
-	
+	var velocity = Vector2(randf_range(150.0, 500.0), 0.0)
 	var direction = mob_spawn_location.rotation + PI / 2
 	
+	mob_spawn_location.progress_ratio = randf()
 	mob.position = mob_spawn_location.position
-	
 	direction += randf_range(-PI/4, PI/4)
 	mob.rotation = direction
-	
-	var velocity = Vector2(randf_range(150.0, 500.0), 0.0)
 	mob.linear_velocity = velocity.rotated(direction)
-	
 	add_child(mob)
 
 func _on_score_timer_timeout() -> void:
